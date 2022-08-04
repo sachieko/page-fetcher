cb697f5ca57782ce51764d18b259bf15c155193d
sachieko <kirazetsubou@gmail.com>
2022-08-04 00:09:48 -0600

    All files implemented and committed.

---
+++ fetcher.js
@@ -0,0 +1,26 @@
+const HOST = process.argv[2];
+const FILEPATH = process.argv[3];
+const fs = require('fs');
+const request = require('request');
+const chalk = require('chalk');
+
+console.log(HOST, FILEPATH);
+
+request(HOST, (error, response, body) => {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  fs.open(FILEPATH, 'wx', (err, fd) => {
+    if (err) {
+      if (err.code === 'EEXIST') {
+        console.error(chalk.red('myfile already exists'));
+        return;
+      }
+  
+      throw err;
+    }
+    fs.write(fd, body, (err) => {
+      if (err) console.error(chalk.bgRed('Scream incoherently because file didn\t write!'));
+      if (!err) console.log(`Downloaded and saved ${chalk.green(body.length)} bytes to ${chalk.magenta(FILEPATH)}`);
+    });
+  });
+});
